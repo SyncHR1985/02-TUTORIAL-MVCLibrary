ae9108f2958c333c64504703c0b0d4df33e41a02
Davor Petrlić <dapetrli@vsite.hr>
2024-11-21 17:41:09 +0100

    Added author field

--- MVCLibrary/Migrations/20241121162411_AddAuthorToBook.Designer.cs
+++ MVCLibrary/Migrations/20241121162411_AddAuthorToBook.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCLibrary.Migrations
 {
     [DbContext(typeof(MVCLibraryContext))]
-    partial class MVCLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20241121162411_AddAuthorToBook")]
+    partial class AddAuthorToBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
